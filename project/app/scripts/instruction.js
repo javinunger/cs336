4320eb350f5169f7d1406d81a5694b1b17076323
Javin Unger <jbu2@dijkstra.cs.calvin.edu>
2016-12-05 13:37:55 -0500

    Sketch of instruction.js created

---
+++ project/app/scripts/instruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import $ from 'jquery';
+
+import Image from './image';
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<div className="Instruction">
+				{"PLACEHOLDER"}
+			</div>
+		);
+	}
+});
