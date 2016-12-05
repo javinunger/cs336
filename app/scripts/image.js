ae1f7097ddae5475bd6a0e00f463d9a7ed341f54
Javin Unger <jbu2@dijkstra.cs.calvin.edu>
2016-12-05 13:45:03 -0500

    werk werk werk

---
+++ app/scripts/image.js
@@ -0,0 +1,8 @@
+/*
+$( "canvas" ).click(function( event ) {
+  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+  $( "span #first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+  $( "span #last" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
+});
+*/
