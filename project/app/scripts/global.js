3889a58e816a699c21ee285d26e9dbe48efa7372
Javin Unger <jbu2@dijkstra.cs.calvin.edu>
2016-12-05 13:06:24 -0500

    Updated scripts & all the backend stuff

---
+++ project/app/scripts/global.js
@@ -0,0 +1,3 @@
+const API_URL = 'api/coordinates';
+
+module.exports = { API_URL };
--- project/server.js
+++ project/server.js
@@ -4,13 +4,14 @@
  * Created and adapted by jbu2 and cjk45 for CS336, Fall 2016
  */
 
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient
 
-var COORDINATES_FILE = path.join(__dirname, 'coordinates.json');
+var db;
+var APP_PATH = path.join(__dirname, 'dist');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -18,24 +19,16 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching.
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
 
-app.get('/api/cooridnates', function(req, res) {
-    fs.readFile(COORDINATES_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
+app.get('/api/coordinates', function(req, res) {
+    db.collection("coordinates").find({}).toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
     });
 });
 
@@ -61,9 +54,34 @@ app.post('/api/coordinates', function(req, res) {
     });
 });
 
+app.post('/api/coordinates', function(req, res) {
+    var newCoordinate = {
+        x: req.body.x,
+        y: req.body.y,
+    };
+    db.collection("coordinates").insertOne(newCoordinate, function(err, result) {
+        if (err) throw err;
+        db.collection("coordinates").find({}).toArray(function(err, docs) {
+            if (err) throw err;
+            res.json(docs);
+        });
+    });
+});
+
+
+// Send all routes/methods not specified above to the app root.
+app.use('*', express.static(APP_PATH));
 
 app.listen(app.get('port'), function() {
     console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
+
+var mongoURL = 'mongodb://cs336:' +
+               process.env.MONGO_PASSWORD +
+               '@ds011495.mlab.com:11495/jbu2-cs336';
+MongoClient.connect(mongoURL, function(err, dbConnection) {
+    if (err) throw err;
+    db = dbConnection;
 });
 
 /**
