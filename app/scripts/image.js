5d36d50ce4b97e550ec3cbb0a182358cd948fb42
javinunger <jbu2@students.calvin.edu>
2016-12-08 15:43:12 -0500

    Add files via upload

--- app/scripts/image.js
+++ app/scripts/image.js
@@ -1,30 +1,36 @@
-/*
-$( "canvas" ).click(function( event ) {
-  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
-  $( "span #first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
-  $( "span #last" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
-});
-*/
+import React from 'react';
+import $ from "jquery";
 
-/*
-handleCoordinateSubmit: function(coordinate) {
-  	var coordinates = this.state.data;
-    coordinate.id = Date.now();
-    var newCoordinates = coordinates.concat([coordinate]);
-    this.setState({data: newCoordinates});
-    $.ajax({
-  	  url: API_URL,
-      dataType: 'json',
-      type: 'POST',
-      data: coordinate,
-    })
-    .done(function(result){
-  		  this.setState({data: result});
-    }.bind(this))
-    .fail(function(xhr, status, errorThrown) {
-    		this.setState({data: coordinates});
-        console.error(API_URL, status, errorThrown.toString());
-    }.bind(this));
-	}
-*/
+
+module.exports = React.createClass({
+    handleCoordinateSubmit: function(coordinate) {
+      	var coordinates = this.state.data;
+        coordinate.id = Date.now();
+        var newCoordinates = coordinates.concat([coordinate]);
+        this.setState({data: newCoordinates});
+        $.ajax({
+      	  url: API_URL,
+          dataType: 'json',
+          type: 'POST',
+          data: coordinate,
+        })
+        .done(function(result){
+      		  this.setState({data: result});
+        }.bind(this))
+        .fail(function(xhr, status, errorThrown) {
+        		this.setState({data: coordinates});
+            console.error(API_URL, status, errorThrown.toString());
+        }.bind(this));
+  	},
+    render: function() {
+        return (
+          <div className="Image">
+              <h3>image component render test</h3>
+              <img id='imageOnCanvas' src="http://lorempixel.com/700/450/"/>
+              <canvas id="imageCanvas" width="700" height="450"></canvas>
+              <p>^This canvas is being rendered by the image component, which is being rendered by container component. (The image is behind the canvas! Check the console for coordinate-click output.)</p>
+              <img id='cursor' src="http://image.flaticon.com/icons/png/128/63/63560.png"/>
+          </div>
+        );
+    }
+});
