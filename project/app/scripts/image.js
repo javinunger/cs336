a0ec37753e342a2eb2d4bf9a3f7ede2efc357474
javinunger <jbu2@students.calvin.edu>
2016-12-17 13:54:15 -0500

    Documentation for image.js

--- project/app/scripts/image.js
+++ project/app/scripts/image.js
@@ -1,6 +1,11 @@
+/* React component for the image
+ * 12/19 - Renders <div> element that contains the actual image and then a <canvas>, which is placed on top of the image.
+ *
+ * Written by Cotter Koopman and Javin Unger for CS336, Fall 2016
+ */
+
 import React from 'react';
 import $ from "jquery";
-
 
 module.exports = React.createClass({
     render: function() {
