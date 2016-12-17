2ec9a964b6cf1133491b4bbaaf27c7d8643c90ef
cotterjk <cjk45@students.calvin.edu>
2016-12-17 13:42:14 -0500

    Update instruction.js
    Added documentation

--- app/scripts/instruction.js
+++ app/scripts/instruction.js
@@ -1,3 +1,10 @@
+/* React component for the instruction text
+ * Creates a class with text that prompts the user to click on a world map.
+ * Rendered by container.js
+ *
+ * Written by Cotter Koopman and Javin Unger for CS336, Fall 2016
+ */
+
 import React from 'react';
 import $ from 'jquery';
 
